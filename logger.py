80b90f111ae98425d60e6aba4c960fb56759ceb8
Sergio Orizano Salvador <svorizano@gmail.com>
2025-02-24 20:49:52 -0500

    quitando directorios

--- logger.py
+++ logger.py
@@ -14,6 +14,6 @@ def setup_logger(name, level=logging.INFO):
     return logger
 
 # Creación de los loggers
-activity_logger = setup_logger("activity", "logs/activity.log", logging.INFO)
-warning_logger = setup_logger("warning", "logs/warning.log", logging.WARNING)
-error_logger = setup_logger("error", "logs/error.log", logging.ERROR)
+activity_logger = setup_logger("activity", logging.INFO)
+warning_logger = setup_logger("warning", logging.WARNING)
+error_logger = setup_logger("error", logging.ERROR)
